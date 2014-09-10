ad526e8ae5295bdb26c1934293a3717fec30f3bd
tomgreenfield <tomgreenfield@users.noreply.github.com>
2014-09-10 11:27:05 +0100

    Updated toggle_white_space

--- toggle_white_space.py
+++ toggle_white_space.py
@@ -2,8 +2,10 @@ import sublime
 import sublime_plugin
 
 class ToggleWhiteSpaceCommand(sublime_plugin.ApplicationCommand):
-    def run(self):
-        settings = sublime.load_settings("Preferences.sublime-settings")
-        white_space = "none" if settings.get("draw_white_space", "none") != "none" else "all"
-        settings.set("draw_white_space", white_space)
-        sublime.save_settings("Preferences.sublime-settings")
+	def run(self):
+		settings = sublime.load_settings("Preferences.sublime-settings")
+		if settings.get("draw_white_space") != "all":
+			settings.set("draw_white_space", "all")
+		else:
+			settings.erase("draw_white_space")
+		sublime.save_settings("Preferences.sublime-settings")
